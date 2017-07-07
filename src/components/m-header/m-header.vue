dbe31dd04d4a2e7e565934022bfa76ed9afa0517
WangQingye <qingye.wang@winasia.io>
2017-07-07 09:56:58 +0800

    header

---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="m-header">
+		<div class="icon"></div>
+		<h1 class="text">Chicken Music</h1>
+	</div>
+</template>
+
+<script type="text/ecmascript-6">
+	export default {}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+	@import "~common/stylus/variable.styl"
+	@import "~common/stylus/mixin.styl"
+
+	.m-header
+		position relative
+		height 44px
+		color $color-theme
+		font-size
+		text-align center
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App.vue'
-Vue.config.productionTip = false
-
+import fastclick from 'fastclick'
 import './common/stylus/index.styl'
 
+fastclick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
